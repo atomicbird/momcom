ded0b3815ea9ef4a42b376c5f2e26978c352a673
Tom Harrington <tph@atomicbird.com>
2013-04-17 17:03:27 -0600

    Initial Commit

---
+++ momc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  momc
+//
+//  Created by Tom Harrington on 4/17/13.
+//  Copyright (c) 2013 Tom Harrington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
