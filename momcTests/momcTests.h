ccc4bf3b1df9d19776aa39fabed55b9ab7239f15
Tom Harrington <tph@atomicbird.com>
2013-04-29 17:09:55 -0600

    Add unit test setup

---
+++ momcTests/momcTests.h
@@ -0,0 +1,13 @@
+//
+//  momcTests.h
+//  momcTests
+//
+//  Created by Tom Harrington on 4/29/13.
+//  Copyright (c) 2013 Tom Harrington. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface momcTests : SenTestCase
+
+@end
---
+++ momcTests/momcTests.m
@@ -0,0 +1,32 @@
+//
+//  momcTests.m
+//  momcTests
+//
+//  Created by Tom Harrington on 4/29/13.
+//  Copyright (c) 2013 Tom Harrington. All rights reserved.
+//
+
+#import "momcTests.h"
+
+@implementation momcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in momcTests");
+}
+
+@end
