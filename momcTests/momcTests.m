850ed3ee9fec3ac8c3b9f89ede3b3d8e39c5576a
Tom Harrington <tph@atomicbird.com>
2013-04-29 18:23:37 -0600

    Build out unit tests to compare compiled models to those generated by Xcode

--- momcTests/momcTests.m
+++ momcTests/momcTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "momcTests.h"
+#import "NSManagedObjectModel+momc.h"
 
 @implementation momcTests
 
@@ -24,9 +25,75 @@
     [super tearDown];
 }
 
-- (void)testExample
+// Work around a bug in xcrun which truncates min/max values on decimal attributes to integer values
+// See rdar://problem/13677527 or http://openradar.appspot.com/radar?id=2948402
+// This method overwrites validation predicates on any decimal attributes to have values found
+// in the attribute's userInfo dictionary, which should be configured to match rdar 13677527 behavior.
+- (void)makeDecimalValidationMatchRadar13677527ForModel:(NSManagedObjectModel *)compiledModel
 {
-    STFail(@"Unit tests are not implemented yet in momcTests");
+    for (NSEntityDescription *entityDescription in [compiledModel entities]) {
+        for (NSString *attributeName in [entityDescription attributesByName]) {
+            NSAttributeDescription *attributeDescription = [[entityDescription attributesByName] objectForKey:attributeName];
+            if ([attributeDescription attributeType] == NSDecimalAttributeType) {
+                NSDictionary *userInfo = [attributeDescription userInfo];
+                NSMutableArray *newValidationPredicates = [NSMutableArray array];
+                NSNumber *rdar13677527min = [NSNumber numberWithInteger:[[userInfo objectForKey:@"rdar13677527min"] integerValue]];
+                if (rdar13677527min != nil) {
+                    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF >= %@", rdar13677527min];
+                    [newValidationPredicates addObject:predicate];
+                }
+                NSNumber *rdar13677527max = [NSNumber numberWithInteger:[[userInfo objectForKey:@"rdar13677527max"] integerValue]];
+                if (rdar13677527max != nil) {
+                    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF <= %@", rdar13677527max];
+                    [newValidationPredicates addObject:predicate];
+                }
+                [attributeDescription setValidationPredicates:newValidationPredicates withValidationWarnings:[attributeDescription validationWarnings]];
+            }
+        }
+    }
+}
+
+- (void)testCompile
+{
+    // Get the uncompiled model path
+    NSBundle *selfBundle = [NSBundle bundleForClass:[self class]];
+    NSString *uncompiledModelPath = [selfBundle pathForResource:@"momcTests" ofType:@"xcdatamodeld"];
+    
+    // Compile the model into a temporary directory
+    NSString *momcTestDir = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"momcTests-%d", getpid()]];
+    [[NSFileManager defaultManager] createDirectoryAtPath:momcTestDir withIntermediateDirectories:YES attributes:0 error:nil];
+    NSString *compiledModelPath = [NSManagedObjectModel compileModelAtPath:uncompiledModelPath inDirectory:momcTestDir];
+    NSManagedObjectModel *compiledModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:[NSURL fileURLWithPath:compiledModelPath]];
+    
+    // For comparison, have xcrun compile the model.
+    NSString *xcrunCompiledModelPath = [momcTestDir stringByAppendingPathComponent:@"momcTests-xcrun.momd"];
+    NSTask *compileTask = [NSTask launchedTaskWithLaunchPath:@"/usr/bin/xcrun" arguments:@[@"momc", uncompiledModelPath, xcrunCompiledModelPath]];
+    [compileTask waitUntilExit];
+    NSManagedObjectModel *xcrunCompiledModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:[NSURL fileURLWithPath:xcrunCompiledModelPath]];
+    
+    // Compare the results.
+    [self makeDecimalValidationMatchRadar13677527ForModel:compiledModel];
+    
+    NSDictionary *compiledEntities = [compiledModel entitiesByName];
+    NSDictionary *xcrunCompiledEntities = [xcrunCompiledModel entitiesByName];
+    for (NSString *entityName in compiledEntities) {
+        NSEntityDescription *compiledEntity = compiledEntities[entityName];
+        NSEntityDescription *xcrunCompiledEntity = xcrunCompiledEntities[entityName];
+        STAssertEqualObjects(compiledEntity, xcrunCompiledEntity, @"Entities do not match: %@", entityName);
+    }
+    
+    NSDictionary *compiledFetchRequests = [compiledModel fetchRequestTemplatesByName];
+    NSDictionary *xcrunCompiledFetchRequests = [xcrunCompiledModel fetchRequestTemplatesByName];
+    for (NSString *fetchRequestName in compiledFetchRequests) {
+        NSFetchRequest *compiledFetchRequest = compiledFetchRequests[fetchRequestName];
+        NSFetchRequest *xcrunCompiledFetchRequest = xcrunCompiledFetchRequests[fetchRequestName];
+        STAssertEqualObjects(compiledFetchRequest, xcrunCompiledFetchRequest, @"Fetch requests do not match: %@", fetchRequestName);
+    }
+    
+    NSArray *compiledConfigurations = [compiledModel configurations];
+    for (NSString *configurationName in compiledConfigurations) {
+        STAssertEqualObjects([compiledModel entitiesForConfiguration:configurationName], [xcrunCompiledModel entitiesForConfiguration:configurationName], @"Configuration does not match: %@", configurationName);
+    }
 }
 
 @end
